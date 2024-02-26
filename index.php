668a247bbde5d8afbfeb5c6587d696d13e32be33
GaranceVermorel <garancevermorel66@gmail.com>
2024-02-26 14:50:05 +0100

    test url

--- index.php
+++ index.php
@@ -13,32 +13,8 @@
 
         <?php
         $url = "https://ddragon.leagueoflegends.com/cdn/12.6.1/data/fr_FR/champion.json";
-        print_r($url);
-
-
-        $ch = curl_init($url);
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-        $resp = curl_exec($ch);
-
-        print_r($resp);
-
-
-        if ($e = curl_error($ch)) {
-            echo $e;
-        } else {
-            $decoded = json_decode($resp);
-            print_r($decoded);
-        }
-
-
-        curl_close($ch);
-
-
+        $data = file_get_contents($url);
+        print_r($data);
         ?>
     </body>
 </html>
