cc1aa94be7aaf7dc8ddd49eb4c49f9c31bf6ebf0
GaranceVermorel <garancevermorel66@gmail.com>
2024-03-11 17:01:45 +0100

    Update header.php

--- skeleton/header.php
+++ skeleton/header.php
@@ -4,5 +4,4 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./css/common.css">
-    </head>
-    <body>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
