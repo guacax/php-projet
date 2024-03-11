66b97f99a0b0c90816cacc2505d1d60b1346c2a8
GaranceVermorel <garancevermorel66@gmail.com>
2024-03-11 17:03:13 +0100

    Update index.php
    indentation

--- index.php
+++ index.php
@@ -1,47 +1,54 @@
+<?php include('./skeleton/header.php') ?>
+
 <link rel="stylesheet" href="./css/index.css">
 <h1>Le loldex de gua</h1>
 <title>Loldex</title>
 
 <h2><a href="items.php" target="_blank">Items</a></h2>
 
-<?php include('./skeleton/header.php') ?>
+<?php
+$url = "https://ddragon.leagueoflegends.com/cdn/14.5.1/data/fr_FR/champion.json";
+
+$response = file_get_contents($url);
+
+$data_decode = json_decode($response, true);
+
+$data = $data_decode['data'];
 
 
-    <?php
-    $url = "https://ddragon.leagueoflegends.com/cdn/14.5.1/data/fr_FR/champion.json";
-
-    $response = file_get_contents($url);
-
-    $data_decode = json_decode($response, true);
-
-    $data = $data_decode['data'];
-
-
-    /*
-     ( [Aatrox] => Array
-        ( [version] => 12.6.1
+/*
+    [Aatrox] => Array
+    (
+        [version] => 12.6.1
         [id] => Aatrox
         [key] => 266
         [name] => Aatrox
         [title] => Épée des Darkin
         [blurb] => Autrefois, Aatrox et ses frères étaient honorés pour avoir défendu Shurima contre le Néant. Mais ils finirent par devenir une menace plus grande encore pour Runeterra : la ruse et la sorcellerie furent employées pour les battre. Cependant, après des...
-        [info] => Array (
+        [info] => Array
+        (
             [attack] => 8 [defense] => 4
             [magic] => 3
-            [difficulty] => 4 )
-        [image] => Array (
+            [difficulty] => 4
+        )
+        [image] => Array
+        (
             [full] => Aatrox.png
             [sprite] => champion0.png
             [group] => champion
             [x] => 0
             [y] => 0
             [w] => 48
-            [h] => 48 )
-        [tags] => Array (
+            [h] => 48
+        )
+        [tags] => Array
+        (
             [0] => Fighter
-            [1] => Tank )
+            [1] => Tank
+        )
         [partype] => Puits de sang
-        [stats] => Array (
+        [stats] => Array
+        (
             [hp] => 580
             [hpperlevel] => 90
             [mp] => 0
@@ -61,27 +68,29 @@
             [attackdamage] => 60
             [attackdamageperlevel] => 5
             [attackspeedperlevel] => 2.5
-            [attackspeed] => 0.651 )
-            )
-        */
-    ?>
+            [attackspeed] => 0.651
+        )
+    )
+*/
+?>
 
-    <main>
-        <?php
+<main>
+    <?php
         foreach($data as $champions => $champion)
         {
-            ?> <div><?php
-            echo $champion['name'] . "<br>";
-            echo $champion['title'] . "<br>";
-            echo $champion['blurb'] . "<br>";
-            $image = $champion['image']; ?>
-            <img src="https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/<?=$champion['id']?>.png"> <br>
-            <p><a href="detail_champion.php?id=<?=$champion['id']?>" target="_blank">Voir plus</a></p>
-            </div>
-        <?php
+            ?> 
+            <div>
+                <?php
+                echo $champion['name'] . "<br>";
+                echo $champion['title'] . "<br>";
+                echo $champion['blurb'] . "<br>";
+                $image = $champion['image']; ?>
+                <img src="https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/<?=$champion['id']?>.png"><br>
+                <p><a href="detail_champion.php?id=<?=$champion['id']?>" target="_blank">Voir plus</a></p>
+                </div>
+            <?php
         }
-        ?>
-
-    </main>
+    ?>
+</main>
 
 <?php include('./skeleton/footer.php') ?>
