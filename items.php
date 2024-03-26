ada31fff2b69eb98547d14091e5c39e9c69fced8
GaranceVermorel <garancevermorel66@gmail.com>
2024-03-26 14:10:22 +0100

    Update items.php
    find origine of an item

--- items.php
+++ items.php
@@ -1,12 +1,10 @@
-<title>Items</title>
-
 <?php
     include('./skeleton/header.php');
-//page pour les items
-//
-//url data : https://ddragon.leagueoflegends.com/cdn/14.5.1/data/fr_FR/item.json
-//
-/*item type :
+    //page pour les items
+    //
+    //url data : https://ddragon.leagueoflegends.com/cdn/14.5.1/data/fr_FR/item.json
+    //
+    /*item type :
     [1001] => Array (
         [name] => Bottes
         [description] => +25 vitesse de déplacement
@@ -33,84 +31,77 @@
         )
         [gold] => Array (
             [base] => 300
-        [purchasable] => 1
-        [total] => 300
-        [sell] => 210
+            [purchasable] => 1
+            [total] => 300
+            [sell] => 210
         )
         [tags] => Array (
             [0] => Boots
         )
         [maps] => Array (
             [11] => 1
-        [12] => 1
-        [21] => 1
-        [22] =>
-        [30] =>
+            [12] => 1
+            [21] => 1
+            [22] =>
+            [30] =>
         )
         [stats] => Array (
             [FlatMovementSpeedMod] => 25
         )
+    ]
     */
-//
-//les into c'est pour savoir quel build possible avec l'item
-//
-//url img : https://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/1001.png
-
-    $url = "https://ddragon.leagueoflegends.com/cdn/14.5.1/data/fr_FR/item.json";
-
-    $response = file_get_contents($url);
-
-    $dataDecoded = json_decode($response, true);
-
-    $data = $dataDecoded['data'];
-
-//  print_r($data);
-
-//    print_r($data);
-
-    foreach ($data as $itemKey => $item){
-        echo $item['name'] . "<br>";
-        ?>
-        <img src="https://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/<?=$itemKey?>.png"> <br>
+?>
+    <link rel="stylesheet" href="./css/items.css/"/>
+    <title>Items</title>
+</head>
+    <body>
         <?php
-echo "tes là ?";
-//        echo $itemKey;
-        if (isset($item['into'])) {
-            $intos = $item['into'];
-            echo count($intos) . "<br>";
+        $url = "https://ddragon.leagueoflegends.com/cdn/14.5.1/data/fr_FR/item.json";
 
-            foreach ($intos as $intoKey => $into) {
-                echo "Peux évo en : " . $data[$into]['name'] . "<br>";
+        $response = file_get_contents($url);
+
+        $dataDecoded = json_decode($response, true);
+
+        $data = $dataDecoded['data'];
+        foreach ($data as $itemKey => $item){
+            echo $item['name'] . "<br>";
+            ?>
+            <img src="https://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/<?=$itemKey?>.png"> <br>
+            <?php
+            if (isset($item['into'])) {
+                $intos = $item['into'];
+                echo count($intos) . "<br>";
+
+                foreach ($intos as $intoKey => $into) {
+                    echo "Peux évo en : " . $data[$into]['name'] . "<br>";
+                    ?>
+                    <img src="https://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/<?=$into?>.png"> <br>
+                    <?php
+                }
+            } else {
+                echo "Pas d'évo possible <br>";
+            }
+            foreach (findOrigin($itemKey, $data) as $itemUpgradeKey => $item) {
+                echo "Peux être obtenu à partir de : " . $data[$item]['name'] . "<br>";
                 ?>
-                <img src="https://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/<?=$into?>.png"> <br>
+                <img src="https://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/<?=$item?>.png"> <br>
                 <?php
             }
-        } else {
-            echo "Pas d'évo possible <br>";
-        }
-        foreach (findOrigin($itemKey, $data) as $itemUpgradeKey => $item) {
-            // var_dump($item);
-            echo "Peux être obtenu à partir de : " . $data[$item]['name'] . "<br>";
-            ?>
-            <img src="https://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/<?=$item?>.png"> <br>
-            <?php
+
+            echo "<br>";
         }
 
-        echo "<br>";
-    }
-
-function findOrigin ($idItem, $data) {
-        $rtn = [];
-    foreach ($data as $itemKey => $item) {
-        if (isset($item['into'])) {
-            if( array_search($idItem, $item['into'])!=false ){
-                array_push($rtn, $itemKey);
+        function findOrigin ($idItem, $data) {
+            $rtn = [];
+            foreach ($data as $itemKey => $item) {
+                if (isset($item['into'])) {
+                    if( array_search($idItem, $item['into'])!=false ){
+                        array_push($rtn, $itemKey);
+                    }
+                }
             }
-        }
-    }
-
-    return $rtn;
-};
+            return $rtn;
+        };
 
     include('./skeleton/footer.php');
 
